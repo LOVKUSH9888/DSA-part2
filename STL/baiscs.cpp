e594fefe41ae30c99dd3df6bff195efa6e81167d
LOVKUSH <lovkushyadav9888@gmail.com>
2022-11-16 23:25:17 +0530

    Basics of STL

---
+++ STL/baiscs.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h> // for adding the stl HERE 
+using namespace std;
+void print(){
+    cout<<"Lovkush";
+}
+int sum (int a, int b){
+    return a+b;
+}
+main(){
+    print();
+    int s = sum(1, 5);
+    cout<<s<<endl;
+}
