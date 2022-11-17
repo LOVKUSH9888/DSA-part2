b3b68d7900ba60d3187a046177c6b1eded75b6c1
LOVKUSH <lovkushyadav9888@gmail.com>
2022-11-17 21:19:19 +0530

    Linear_Search

---
+++ SEARCHING/linear_Search.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0; i<n; i++>){
+        int target;
+        cin>>target;
+
+        int flag = 0; // Taking flagging variable
+        for(int i= 0; i<n; i++){
+            if(a[]==target){
+                flag = 1;
+                break;
+            }
+        }
+        if(flag==1) cout<<"true";
+        else cout<<"false";
+    }
+}
